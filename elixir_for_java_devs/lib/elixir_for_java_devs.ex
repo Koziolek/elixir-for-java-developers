0bc4d38b80056e4d0dbdbe735ffdd1700eacceea
Koziolek <bjkuczynski@gmail.com>
2017-04-02 17:36:46 +0200

    Init

---
+++ elixir_for_java_devs/lib/elixir_for_java_devs.ex
@@ -0,0 +1,18 @@
+defmodule ElixirForJavaDevs do
+  @moduledoc """
+  Documentation for ElixirForJavaDevs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirForJavaDevs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
