e363db4cdf9c2691c35ee0f740fc1bbda5f3efe3
Koziolek <bjkuczynski@gmail.com>
2017-04-15 20:18:33 +0200

    Add spawn monitor

--- elixir_for_java_devs/lib/elixir_for_java_devs.ex
+++ elixir_for_java_devs/lib/elixir_for_java_devs.ex
@@ -26,8 +26,17 @@ defmodule ElixirForJavaDevs do
   def listen2 do
     receive do
         {:ok, "Hi"} -> IO.puts "HI!"
+        {:ok, "Hello"} -> IO.puts "Hello!"
         _ -> exit(:kboom)
     end
     listen2()
   end
+
+  def run do
+      {pid, ref} = spawn_monitor(ElixirForJavaDevs, :listen2, [])
+      receive do
+        {:DOWN, ref, :process, from_pid, reason} -> IO.puts "Exit reason: #{reason}"
+      end
+  end
+
 end
